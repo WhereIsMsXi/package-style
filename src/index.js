8152e78e67187fb66f991160b691bf55ba03ed03
WhereIsMsXi <whereismsxi@gmail.com>
2024-08-15 17:01:43 +0800

    获取并拼接 scss

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
-function print() {
-  console.log('print'); 
+const path = require('path');
+const fs = require('fs').promises;
+
+const inputDir = './src/styles';
+
+async function concatSass(inputDir) {
+  let result = ''
+
+  const scssFiles = await fs.readdir(inputDir);
+  for( const file of scssFiles ) {
+    if(path.extname(file) === '.scss') {
+      const filePath = path.join(inputDir, file);
+      const content = await fs.readFile(filePath, 'utf8');
+      result += content + '\n';
+    }
+  }
+
+  return result;
 }
+
+async function boot() {
+  let content = '';
+
+  content = await concatSass(inputDir);
+}
+
+boot();
